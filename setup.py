504a97b75cc1339772f470d1ae58e7500ce36611
h1nk <12993584+h1nk@users.noreply.github.com>
2019-08-20 22:08:49 -0400

    Version 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hink',
-    version=None,
+    version='0.0.1',
     author='hink',
     url='https://github.com/h1nk/django-recipes',
     packages=find_packages(exclude=['tests', 'docs']),
