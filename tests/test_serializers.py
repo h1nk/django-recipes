e72947a8fa561a15028d76d9d218a32393e368b1
h1nk <12993584+h1nk@users.noreply.github.com>
2019-08-20 22:29:37 -0400

    Clean up MsgPack test a bit

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -1,8 +1,9 @@
+from unittest import TestCase
 from random import getrandbits
 from ipaddress import ip_address
 
 from django.core import signing
-from django.test import TestCase
+from django.core.signing import JSONSerializer
 
 from django_recipes import MsgPackSerializer
 
@@ -12,9 +13,7 @@ class TestMsgPackSerializer(TestCase):
         data = [getrandbits(64), int(ip_address(getrandbits(32)))]
 
         msgpack_data = signing.dumps(data, serializer=MsgPackSerializer)
-        msgpack_data = msgpack_data.replace(':', '.')
+        json_data = signing.dumps(data, serializer=JSONSerializer)
 
-        json_data = signing.dumps(data)
-
+        self.assertEqual(data, signing.loads(msgpack_data, serializer=MsgPackSerializer))
         self.assertLessEqual(len(msgpack_data), len(json_data))
-        self.assertEqual(data, signing.loads(msgpack_data.replace('.', ':'), serializer=MsgPackSerializer))
