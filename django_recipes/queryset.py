96c3f32950d23313ddf3980829663af44b45336e
h1nk <12993584+h1nk@users.noreply.github.com>
2020-10-07 02:50:41 -0400

    Copy-pasted RedisList

--- django_recipes/queryset.py
+++ django_recipes/queryset.py
@@ -1,3 +1,5 @@
+import pickle
+
 from redis import Redis
 
 __all__ = (
@@ -27,3 +29,19 @@ class RedisSortedSet:
     def __getitem__(self, item):
         if isinstance(item, slice):
             return self.redis.zrange(self.key, item.start, item.stop - 1, desc=self.desc, withscores=self.withscores)
+
+
+class RedisList:
+    def __init__(self, *, redis: Redis, key: str, pickled: bool = False):
+        self.redis: Redis = redis
+        self.key = key
+        self.pickled = pickled
+
+    def __len__(self) -> int:
+        return self.redis.llen(self.key)
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            item = self.redis.lrange(self.key, item.start, item.stop - 1)
+
+            return item if not self.pickled else [pickle.loads(i) for i in item]
